fc45be1da965408a2457d9b1b82a1b5e012cd165
dorababu067 <dorababua67@gmail.com>
2021-07-24 17:38:48 +0530

    initial commit

--- apikey.py
+++
@@ -1,3 +0,0 @@
-SENDGRID_API_KEY=['Get Your API key from sendgrid']
-TO_EMAIL = 'mail@examples.com'
-FROM_EMAIL = 'mail2@examples.com'
--- scraper.py
+++ scraper.py
@@ -3,83 +3,82 @@ import requests
 import pandas as pd
 import base64
 import os.path
-from apikey import *
-from sendgrid import SendGridAPIClient
-from sendgrid.helpers.mail import (Mail, Attachment, FileContent, FileName, FileType, Disposition, ContentId)
 import schedule
 import time
-from datetime import datetime
-import pytz
+import smtplib
+from email.message import EmailMessage
+from decouple import config
+
 
 def job():
-    prices=[]
-    stars=[]
-    titles=[]
-    urlss=[]
+    prices = []
+    stars = []
+    titles = []
+    urlss = []
 
-    pages_to_scrape=10
-    pages=[('http://books.toscrape.com/catalogue/page-{}.html').format(i) for i in range(1,pages_to_scrape+1)]
+    pages_to_scrape = 10
+    pages = [
+        ("http://books.toscrape.com/catalogue/page-{}.html").format(i)
+        for i in range(1, pages_to_scrape + 1)
+    ]
 
     for item in pages:
         page = requests.get(item)
-        soup = bs4(page.text, 'html.parser')
-        for i in soup.findAll('h3'):
+        soup = bs4(page.text, "html.parser")
+        for i in soup.findAll("h3"):
             titles.append(i.getText())
-        for j in soup.findAll('p', class_='price_color'):
+        for j in soup.findAll("p", class_="price_color"):
             prices.append(j.getText())
-        for s in soup.findAll('p', class_='star-rating'):
-            for k,v in s.attrs.items():
+        for s in soup.findAll("p", class_="star-rating"):
+            for k, v in s.attrs.items():
                 stars.append(v[1])
-        divs =soup.findAll('div', class_='image_container')
+        divs = soup.findAll("div", class_="image_container")
         for thumbs in divs:
-            tgs=thumbs.find('img',class_='thumbnail')
-            urls='http://books.toscrape.com/'+str(tgs['src'])
-            newurls=urls.replace("../","")
+            tgs = thumbs.find("img", class_="thumbnail")
+            urls = "http://books.toscrape.com/" + str(tgs["src"])
+            newurls = urls.replace("../", "")
             urlss.append(newurls)
-    data={'Title': titles, 'Prices': prices, 'Stars':stars, "URLs":urlss}
-    df=pd.DataFrame(data=data)
-    df.index+=1
+    data = {"Title": titles, "Prices": prices, "Stars": stars, "URLs": urlss}
+    df = pd.DataFrame(data=data)
+    df.index += 1
     directory = os.path.dirname(os.path.realpath(__file__))
     filename = "scrapedfile.csv"
-    file_path = os.path.join(directory,'csvfiles/', filename)
+    file_path = os.path.join(directory, "csvfiles/", filename)
     df.to_csv(file_path)
 
-    with open(file_path, 'rb') as f:
-        data = f.read()
+    # sendig mail with csv file
+    msg = EmailMessage()
+    msg["Subject"] = "New Scrapped Data"
+    msg["From"] = config("FROM_EMAIL")
+    msg["To"] = config("TO_EMAIL")
+    msg.set_content("Hi, Check the the below attachment for latest scrapped data.")
+
+    # reading the file data
+    with open(file_path, "rb") as f:
+        file_data = f.read()
         f.close()
-
-    encoded = base64.b64encode(data).decode()
-    message = Mail(
-    from_email=FROM_EMAIL,
-    to_emails=TO_EMAIL,
-    subject='Your File is Ready',
-    html_content='<strong>Attached is Your Scraped File</strong>')
-    attachment = Attachment()
-    attachment.file_content = FileContent(encoded)
-    attachment.file_type = FileType('text/csv')
-    attachment.file_name = FileName('scraped.csv')
-    attachment.disposition = Disposition('attachment')
-    attachment.content_id = ContentId('Example Content ID')
-    message.attachment = attachment
+    msg.add_attachment(
+        file_data,
+        maintype="application",
+        subtype="octet_stream",
+        filename="scrapedfile",
+    )
     try:
-        sg = SendGridAPIClient(SENDGRID_API_KEY)
-        response = sg.send(message)
-        print(response.status_code)
-        print(response.body)
-        print(response.headers)
+        with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
+            server.login(config("FROM_EMAIL"), config("PASSWORD"))
+            server.send_message(msg)
     except Exception as e:
-         print(e)
+        print(e)
 
 
-
-# schedule.every(10).minutes.do(job)
-# schedule.every().hour.do(job)
-schedule.every().day.at('13:58').do(job)
-# schedule.every(5).to(10).minutes.do(job)
-# schedule.every().monday.do(job)
-# schedule.every().wednesday.at("13:15").do(job)
-# schedule.every().minute.at(":17").do(job)
+schedule.every(1).minutes.do(job)
+# # schedule.every().hour.do(job)
+# # schedule.every().day.at('13:58').do(job)
+# # schedule.every(5).to(10).minutes.do(job)
+# # schedule.every().monday.do(job)
+# # schedule.every().wednesday.at("13:15").do(job)
+# # schedule.every().minute.at(":17").do(job)
 
 while True:
     schedule.run_pending()
-    time.sleep(1) # wait one minute
+    time.sleep(1)  # wait one minute
